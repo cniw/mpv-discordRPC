d282964f5dfc05a7d3670fd6923090a1f1d8239b
Wachid Adi Nugroho <wachidadinugroho.maya@gmail.com>
2018-05-23 14:33:33 +0700

    Give double quote on some variable to prevent globbing and word splitting

--- install-linux.sh
+++ install-linux.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 NAME="mpv-discordRPC"
-DIRNAME=$(dirname $0)
+DIRNAME=$(dirname "$0")
 TARGETDIR=${HOME}/.config/mpv/scripts
 
-if  [ ! -d ${TARGETDIR} ] ; then
-    mkdir -p ${TARGETDIR}
+if  [ ! -d "${TARGETDIR}" ] ; then
+    mkdir -p "${TARGETDIR}"
 fi
-cd ${DIRNAME}
+cd "${DIRNAME}"
 
 echo "[${NAME}] installing dependency"
 echo "[${NAME}] ├── discord-rpc"
@@ -18,7 +18,7 @@ fi
 echo "[${NAME}] │   ├── extracting 'discord-rpc-linux.zip'"
 unzip -q discord-rpc-linux.zip
 echo "[${NAME}] │   └── installing 'libdiscord-rpc.so'"
-cp ./discord-rpc/linux-dynamic/lib/libdiscord-rpc.so ${HOME}/.config/mpv/scripts
+cp ./discord-rpc/linux-dynamic/lib/libdiscord-rpc.so "${HOME}"/.config/mpv/scripts
 rm -rf ./discord-rpc
 
 echo "[${NAME}] ├── lua-discordRPC"
@@ -27,7 +27,7 @@ echo "[${NAME}] │   ├── downloading 'discordRPC.lua'"
     wget -q -c "https://github.com/pfirsich/lua-discordRPC/raw/master/discordRPC.lua"
 fi
 echo "[${NAME}] │   └── installing 'discordRPC.lua'"
-cp ./discordRPC.lua ${TARGETDIR}
+cp ./discordRPC.lua "${TARGETDIR}"
 
 echo "[${NAME}] └── status-line"
 if [ ! -f ./status-line.lua ]; then
@@ -35,7 +35,7 @@ echo "[${NAME}]     ├── downloading 'status-line.lua'"
     wget -q -c "https://github.com/mpv-player/mpv/raw/master/TOOLS/lua/status-line.lua"
 fi
 echo "[${NAME}]     └── installing 'status-line.lua'"
-cp ./status-line.lua ${TARGETDIR}
+cp ./status-line.lua "${TARGETDIR}"
 
 echo "[${NAME}] installing main script"
 if [ ! -f ./mpv-discordRPC.lua ]; then
@@ -43,7 +43,7 @@ echo "[${NAME}] ├── downloading 'mpv-discordRPC.lua'"
     wget -q -c "https://github.com/cniw/mpv-discordRPC/raw/master/mpv-discordRPC.lua"
 fi
 echo "[${NAME}] └── installing 'mpv-discordRPC.lua'"
-cp ./mpv-discordRPC.lua ${TARGETDIR}
+cp ./mpv-discordRPC.lua "${TARGETDIR}"
 
 echo "[${NAME}] updating library path"
 sudo sh -c 'echo '"${TARGETDIR}"' > /etc/ld.so.conf.d/'"${NAME}"'.conf'
