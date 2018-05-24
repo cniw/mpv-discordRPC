5b962a572ee6a756a2c37b19e5ca8dab82e9cd65
Wachid Adi Nugroho <wachidadinugroho.maya@gmail.com>
2018-05-24 23:30:24 +0700

    Change library directory

--- install-linux.sh
+++ install-linux.sh
@@ -2,10 +2,14 @@
 
 NAME="mpv-discordRPC"
 DIRNAME=$(dirname "$0")
-TARGETDIR=${HOME}/.config/mpv/scripts
+SCRIPTDIR=${HOME}/.config/mpv/scripts
+LIBDIR=/usr/local/lib
 
-if  [ ! -d "${TARGETDIR}" ] ; then
-    mkdir -p "${TARGETDIR}"
+if  [ ! -d "${SCRIPTDIR}" ] ; then
+    mkdir -p "${SCRIPTDIR}"
+fi
+if  [ ! -d "${LIBDIR}" ] ; then
+    sudo mkdir -p "${LIBDIR}"
 fi
 cd "${DIRNAME}"
 
@@ -18,7 +22,7 @@ fi
 echo "[${NAME}] │   ├── extracting 'discord-rpc-linux.zip'"
 unzip -q discord-rpc-linux.zip
 echo "[${NAME}] │   └── installing 'libdiscord-rpc.so'"
-cp ./discord-rpc/linux-dynamic/lib/libdiscord-rpc.so "${HOME}"/.config/mpv/scripts
+cp ./discord-rpc/linux-dynamic/lib/libdiscord-rpc.so "${LIBDIR}"
 rm -rf ./discord-rpc
 
 echo "[${NAME}] ├── lua-discordRPC"
@@ -27,7 +31,7 @@ echo "[${NAME}] │   ├── downloading 'discordRPC.lua'"
     wget -q -c "https://github.com/pfirsich/lua-discordRPC/raw/master/discordRPC.lua"
 fi
 echo "[${NAME}] │   └── installing 'discordRPC.lua'"
-cp ./discordRPC.lua "${TARGETDIR}"
+cp ./discordRPC.lua "${SCRIPTDIR}"
 
 echo "[${NAME}] └── status-line"
 if [ ! -f ./status-line.lua ]; then
@@ -35,7 +39,7 @@ echo "[${NAME}]     ├── downloading 'status-line.lua'"
     wget -q -c "https://github.com/mpv-player/mpv/raw/master/TOOLS/lua/status-line.lua"
 fi
 echo "[${NAME}]     └── installing 'status-line.lua'"
-cp ./status-line.lua "${TARGETDIR}"
+cp ./status-line.lua "${SCRIPTDIR}"
 
 echo "[${NAME}] installing main script"
 if [ ! -f ./mpv-discordRPC.lua ]; then
@@ -43,10 +47,10 @@ echo "[${NAME}] ├── downloading 'mpv-discordRPC.lua'"
     wget -q -c "https://github.com/cniw/mpv-discordRPC/raw/master/mpv-discordRPC.lua"
 fi
 echo "[${NAME}] └── installing 'mpv-discordRPC.lua'"
-cp ./mpv-discordRPC.lua "${TARGETDIR}"
+cp ./mpv-discordRPC.lua "${SCRIPTDIR}"
 
 echo "[${NAME}] updating library path"
-sudo sh -c 'echo '"${TARGETDIR}"' > /etc/ld.so.conf.d/'"${NAME}"'.conf'
+sudo sh -c 'echo '"${LIBDIR}"' > /etc/ld.so.conf.d/'"${NAME}"'.conf'
 sudo ldconfig
 
 echo -e "\n[discordapp] wachidadinugroho#7674: All done. Good Luck and have a nice day.\n"
