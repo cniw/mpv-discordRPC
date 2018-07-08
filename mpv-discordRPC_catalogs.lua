d031bbbae8612e62ae55b013b61aabcbf02e068d
Wachid Adi Nugroho <wachidadinugroho.maya@gmail.com>
2018-07-08 13:28:25 +0700

    Adding catalogs `mpv-discordRPC_catalogs.lua` for `cover_art` option

---
+++ mpv-discordRPC_catalogs.lua
@@ -0,0 +1,33 @@
+return {
+	{ number = "customCatalog-1", album = { "Abdulrahman Alsudaes Musshaf", "http://www.assabile.com/" }, artist = { "Abdulrahman Alsudaes", "Abdul Rahman Al Sudais" } },
+	{ number = "SVWC-70166~7", album = { "デュラララッピング!! -「デュラララ!!」BEST-", "DRRR Wrapping!! -DuRaRaRa!! BEST-" }, artist = { "THEATRE BROOK", "Matsushita Yuya", "ROOKiEZ is PUNK'D", "ON/OFF", "OKAMOTO’S", "THREE LIGHTS DOWN KINGS", "Toshiyuki Toyonaga", "REVALCY", "FLOW", "PENGUIN RESEARCH" } },
+	{ number = "RR 7655-5", album = { "Resolution" }, artist = { "Lamb of God", "Lamb Of God", "LAMB OF GOD", "lamb of god" } },
+	-- ONE OK ROCK
+	{ number = "AZCL-10011", album = { "ゼイタクビョウ", "Zeitakubyō", "Zeitakubyo" }, artist = { "ONE OK ROCK" } },
+	{ number = "AZCL-10017", album = { "BEAM OF LIGHT", "Beam of Light" }, artist = { "ONE OK ROCK" } },
+	{ number = "AZCB-1001", album = { "感情エフェクト", "Kanjō Effect", "Kanjo Effect" }, artist = { "ONE OK ROCK" } },
+	{ number = "AZCS-1005", album = { "Nicheシンドローム", "Niche Syndrome", "NICHE シンドローム" }, artist = { "ONE OK ROCK" } },
+	{ number = "AZCS-1016", album = { "残響リファレンス", "Zankyō Reference", "Zankyo Reference", "Zankyō Rifarensu", "Zankyo Rifarensu" }, artist = { "ONE OK ROCK" } },
+	{ number = "AZCS-1024", album = { "人生×僕=", "Jinsei×Boku=", "Jinsei × Boku =", "JINSEI KAKETE BOKU WA", "Jinsei Kakete Boku wa" }, artist = { "ONE OK ROCK" } },
+	{ number = "AZCS-1041", album = { "35xxxv" }, artist = { "ONE OK ROCK" } },
+	{ number = "551401-2", album = { "35xxxv (Deluxe Edition)" }, artist = { "ONE OK ROCK" } },
+	{ number = "AZCS-1062", album = { "Ambitions", "Ambitions (Japanese version)" }, artist = { "ONE OK ROCK" } },
+	{ number = "558469-2", album = { "Ambitions (English version)", "Ambitions (International version)", "Ambitions (English ver.)", "Ambitions (International ver.)" }, artist = { "ONE OK ROCK" } },
+	-- DragonForce
+	{ number = "82310-74084-2", album = { "Valley of the Damned", "Valley Of The Damned" }, artist = { "DragonForce", "Dragonforce", "DRAGONFORCE", "dragonforce" } },
+	{ number = "82310-74142-2", album = { "Sonic Firestorm" }, artist = { "DragonForce", "Dragonforce", "DRAGONFORCE", "dragonforce" } },
+	{ number = "168 618 034-2", album = { "Inhuman Rampage" }, artist = { "DragonForce", "Dragonforce", "DRAGONFORCE", "dragonforce" } },
+	{ number = "0602517787797", album = { "Ultra Beatdown" }, artist = { "DragonForce", "Dragonforce", "DRAGONFORCE", "dragonforce" } },
+	{ number = "EG5754", album = { "The Power Within" }, artist = { "DragonForce", "Dragonforce", "DRAGONFORCE", "dragonforce" } },
+	{ number = "SR1062", album = { "Maximum Overload" }, artist = { "DragonForce", "Dragonforce", "DRAGONFORCE", "dragonforce" } },
+	{ number = "WPCR-17718", album = { "Reaching Into Infinity" }, artist = { "DragonForce", "Dragonforce", "DRAGONFORCE", "dragonforce" } },
+	-- Linkin Park
+	{ number = "9 47755-2", album = { "Hybrid Theory" }, artist = { "Linkin Park", "LINKIN PARK", "linkin park" } },
+	{ number = "9362 48444 2", album = { "Meteora" }, artist = { "Linkin Park", "LINKIN PARK", "linkin park" } },
+	{ number = "44477-2", album = { "Minutes to Midnight", "Minutes To Midnight" }, artist = { "Linkin Park", "LINKIN PARK", "linkin park" } },
+	{ number = "525375-2", album = { "A Thousand Suns" }, artist = { "Linkin Park", "LINKIN PARK", "linkin park" } },
+	{ number = "531345-2", album = { "Living Things" }, artist = { "Linkin Park", "LINKIN PARK", "linkin park" } },
+	{ number = "543028-2", album = { "The Hunting Party" }, artist = { "Linkin Park", "LINKIN PARK", "linkin park" } },
+	{ number = "559687-2", album = { "One More Light" }, artist = { "Linkin Park", "LINKIN PARK", "linkin park" } },
+}
+
